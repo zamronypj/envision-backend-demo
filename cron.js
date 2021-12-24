5ded95f8bece8e68082b4ec066685e50b1dca493
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-12-24 09:41:29 +0800

    remove tmpcode

--- cron.js
+++ cron.js
@@ -6,7 +6,7 @@ const scheduleBirthdayMessageTask = require('./crons/scheduleBirthdayMessage')
 
 //run task every hour to make sure we can deliver message at 9am of
 //user local timezone
-cron.schedule('* * * * *', sendMessageTask.run())
+cron.schedule('0 * * * *', sendMessageTask.run())
 
 //run task every day to collect upcoming birthdays
-cron.schedule('* * * * *', scheduleBirthdayMessageTask.run())
+cron.schedule('0 0 * * *', scheduleBirthdayMessageTask.run())
